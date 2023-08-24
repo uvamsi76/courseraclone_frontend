58c81410850d36740e0017b4ee1743beca9f2267
vamsi <uvamsi76@gmail.com>
2023-08-25 00:54:57 +0530

    added the api name

--- src/assets/var.ts
+++ src/assets/var.ts
@@ -1,5 +1,5 @@
 export const lh="http://localhost:3000";
-export const ec2="api.uvlearns.com"
+export const ec2="api.uvlearns.com:3000"
 //"http://ec2-13-51-13-205.eu-north-1.compute.amazonaws.com:3000";
 
 // style={{width:"30%",height:"100%"}}
